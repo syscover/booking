c588aa8efc9329f5b2df62fbdeeb2862205b776f
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-19 15:08:50 +0200

    various changes

---
+++ src/Syscover/Booking/Controllers/VoucherController.php
@@ -0,0 +1,190 @@
+<?php namespace Syscover\Booking\Controllers;
+
+use Syscover\Pulsar\Controllers\Controller;
+use Syscover\Pulsar\Traits\TraitController;
+
+/**
+ * Class VoucherController
+ * @package Syscover\Crm\Controllers
+ */
+
+class VoucherController extends Controller {
+
+    use TraitController;
+
+    protected $routeSuffix  = 'crmCustomer';
+    protected $folder       = 'customer';
+    protected $package      = 'crm';
+    protected $aColumns     = ['id_301', 'name_301', 'surname_301', ['data' => 'email_301', 'type' => 'email'], 'name_300', ['data' => 'active_301', 'type' => 'active'], ['data' => 'confirmed_301', 'type' => 'active']];
+    protected $nameM        = 'name_300';
+    protected $model        = Customer::class;
+    protected $icon         = 'fa fa-user';
+    protected $objectTrans  = 'customer';
+
+    public function customIndex($parameters)
+    {
+        // init record on tap 4
+        $parameters['urlParameters']['tab']     = 1;
+
+        return $parameters;
+    }
+
+    public function customActionUrlParameters($actionUrlParameters, $parameters)
+    {
+        $actionUrlParameters['tab'] = 1;
+
+        return $actionUrlParameters;
+    }
+
+    public function createCustomRecord($parameters)
+    {
+        $parameters['langs']        = Lang::all();
+
+        $parameters['groups']       = Group::all();
+
+        $parameters['genres']       = array_map(function($object) {
+            $object->name = trans($object->name);
+            return $object;
+        }, config('pulsar.genres'));
+
+        $parameters['treatments']   = array_map(function($object) {
+            $object->name = trans($object->name);
+            return $object;
+        }, config('pulsar.treatments'));
+
+        $parameters['states']       = array_map(function($object) {
+            $object->name = trans($object->name);
+            return $object;
+        }, config('pulsar.states'));
+
+        $parameters['attachmentFamilies']   = AttachmentFamily::getAttachmentFamilies(['resource_015' => 'cms-article']);
+        $parameters['attachmentsInput']     = json_encode([]);
+
+        if(isset($parameters['id']))
+        {
+            // get attachments from base lang
+            $attachments = AttachmentLibrary::getRecords($this->package, 'cms-article', $parameters['id'], session('baseLang')->id_001, true);
+
+            // merge parameters and attachments array
+            $parameters  = array_merge($parameters, $attachments);
+        }
+
+        return $parameters;
+    }
+
+    public function storeCustomRecord($parameters)
+    {
+        $customer = Customer::create([
+            'lang_301'                  => $this->request->input('lang'),
+            'group_301'                 => $this->request->input('group'),
+            'date_301'                  => $this->request->has('date')? \DateTime::createFromFormat(config('pulsar.datePattern'), $this->request->input('date'))->getTimestamp() : null,
+            'company_301'               => empty($this->request->input('company'))? null : $this->request->input('company'),
+            'tin_301'                   => empty($this->request->input('tin'))? null : $this->request->input('tin'),
+            'gender_301'                => $this->request->has('gender')? $this->request->input('gender') : null,
+            'treatment_301'             => $this->request->has('treatment')? $this->request->input('treatment') : null,
+            'state_301'                 => $this->request->has('state')? $this->request->input('state') : null,
+            'name_301'                  => empty($this->request->input('name'))? null : $this->request->input('name'),
+            'surname_301'               => empty($this->request->input('surname'))? null : $this->request->input('surname'),
+            'avatar_301'                => empty($this->request->input('avatar'))? null : $this->request->input('avatar'),
+            'birth_date_301'            => $this->request->has('birthDate')? \DateTime::createFromFormat(config('pulsar.datePattern'), $this->request->input('birthDate'))->getTimestamp() : null,
+            'email_301'                 => $this->request->input('email'),
+            'phone_301'                 => empty($this->request->input('phone'))? null : $this->request->input('phone'),
+            'mobile_301'                => empty($this->request->input('mobile'))? null : $this->request->input('mobile'),
+            'user_301'                  => $this->request->input('user'),
+            'password_301'              => Hash::make($this->request->input('password')),
+            'active_301'                => $this->request->has('active'),
+            'confirmed_301'             => false,
+            'country_301'               => $this->request->has('country')? $this->request->input('country') : null,
+            'territorial_area_1_301'    => $this->request->has('territorialArea1')? $this->request->input('territorialArea1') : null,
+            'territorial_area_2_301'    => $this->request->has('territorialArea2')? $this->request->input('territorialArea2') : null,
+            'territorial_area_3_301'    => $this->request->has('territorialArea3')? $this->request->input('territorialArea3') : null,
+            'cp_301'                    => empty($this->request->input('cp'))? null : $this->request->input('cp'),
+            'locality_301'              => empty($this->request->input('locality'))? null : $this->request->input('locality'),
+            'address_301'               => empty($this->request->input('address'))? null : $this->request->input('address'),
+            'latitude_301'              => empty($this->request->input('latitude'))? null : $this->request->input('latitude'),
+            'longitude_301'             => empty($this->request->input('longitude'))? null : $this->request->input('longitude'),
+        ]);
+
+        // set attachments
+        $attachments = json_decode($this->request->input('attachments'));
+        AttachmentLibrary::storeAttachments($attachments, 'crm', 'crm-customer', $customer->id_301, base_lang()->id_001);
+    }
+
+    public function editCustomRecord($parameters)
+    {
+        $parameters['langs']        = Lang::all();
+
+        $parameters['groups']       = Group::all();
+
+        $parameters['genres']       = array_map(function($object){
+            $object->name = trans($object->name);
+            return $object;
+        }, config('pulsar.genres'));
+
+        $parameters['treatments']   = array_map(function($object) {
+            $object->name = trans($object->name);
+            return $object;
+        }, config('pulsar.treatments'));
+
+        $parameters['states']       = array_map(function($object) {
+            $object->name = trans($object->name);
+            return $object;
+        }, config('pulsar.states'));
+
+        // get attachments elements
+        $attachments = AttachmentLibrary::getRecords('crm', 'crm-customer', $parameters['object']->id_301, base_lang()->id_001);
+
+        // merge parameters and attachments array
+        $parameters['attachmentFamilies']   = AttachmentFamily::getAttachmentFamilies(['resource_015' => 'cms-article']);
+        $parameters                         = array_merge($parameters, $attachments);
+
+        return $parameters;
+    }
+
+    public function checkSpecialRulesToUpdate($parameters)
+    {
+        $customer = Customer::builder()->find($parameters['id']);
+
+        $parameters['specialRules']['emailRule']    = $this->request->input('email') == $customer->email_301? true : false;
+        $parameters['specialRules']['userRule']     = $this->request->input('user') == $customer->user_301? true : false;
+        $parameters['specialRules']['passRule']     = ! $this->request->has('password');
+
+        return $parameters;
+    }
+
+    public function updateCustomRecord($parameters)
+    {
+        $customer = [
+            'lang_301'                  => $this->request->input('lang'),
+            'group_301'                 => $this->request->input('group'),
+            'date_301'                  => $this->request->has('date')? \DateTime::createFromFormat(config('pulsar.datePattern'), $this->request->input('date'))->getTimestamp() : null,
+            'company_301'               => empty($this->request->input('company'))? null : $this->request->input('company'),
+            'tin_301'                   => empty($this->request->input('tin'))? null : $this->request->input('tin'),
+            'gender_301'                => $this->request->has('gender')? $this->request->input('gender') : null,
+            'treatment_301'             => $this->request->has('treatment')? $this->request->input('treatment') : null,
+            'state_301'                 => $this->request->has('state')? $this->request->input('state') : null,
+            'name_301'                  => empty($this->request->input('name'))? null : $this->request->input('name'),
+            'surname_301'               => empty($this->request->input('surname'))? null : $this->request->input('surname'),
+            'avatar_301'                => empty($this->request->input('avatar'))? null : $this->request->input('avatar'),
+            'birth_date_301'            => $this->request->has('birthDate')? \DateTime::createFromFormat(config('pulsar.datePattern'), $this->request->input('birthDate'))->getTimestamp() : null,
+            'phone_301'                 => empty($this->request->input('phone'))? null : $this->request->input('phone'),
+            'mobile_301'                => empty($this->request->input('phone'))? null : $this->request->input('mobile'),
+            'active_301'                => $this->request->has('active'),
+            'country_301'               => $this->request->has('country')? $this->request->input('country') : null,
+            'territorial_area_1_301'    => $this->request->has('territorialArea1')? $this->request->input('territorialArea1') : null,
+            'territorial_area_2_301'    => $this->request->has('territorialArea2')? $this->request->input('territorialArea2') : null,
+            'territorial_area_3_301'    => $this->request->has('territorialArea3')? $this->request->input('territorialArea3') : null,
+            'cp_301'                    => empty($this->request->input('cp'))? null : $this->request->input('cp'),
+            'locality_301'              => empty($this->request->input('locality'))? null : $this->request->input('locality'),
+            'address_301'               => empty($this->request->input('address'))? null : $this->request->input('address'),
+            'latitude_301'              => empty($this->request->input('latitude'))? null : $this->request->input('latitude'),
+            'longitude_301'             => empty($this->request->input('longitude'))? null : $this->request->input('longitude'),
+        ];
+
+        if($parameters['specialRules']['emailRule'])  $customer['email_301']      = $this->request->input('email');
+        if($parameters['specialRules']['userRule'])   $customer['user_301']       = $this->request->input('user');
+        if(! $parameters['specialRules']['passRule']) $customer['password_301']   = Hash::make($this->request->input('password'));
+
+        Customer::where('id_301', $parameters['id'])->update($customer);
+    }
+}
---
+++ src/Syscover/Booking/Models/Voucher.php
@@ -0,0 +1,159 @@
+<?php namespace Syscover\Booking\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Validator;
+use Syscover\Pulsar\Traits\TraitModel;
+use Sofa\Eloquence\Eloquence;
+use Sofa\Eloquence\Mappable;
+
+/**
+ * Class Voucher
+ *
+ * Model with properties
+ * <br><b>[id, lang, group, date, company, tin, gender, treatment, state, name, surname, avatar, birth_date, email, phone, mobile, user, password, active, confirmed, country, territorial_area_1, territorial_area_2, territorial_area_3, cp, locality, address, latitude, longitude, custom_field_group, data]</b>
+ *
+ * @package     Syscover\Crm\Models
+ */
+
+class Voucher extends Authenticatable
+{
+    use TraitModel;
+    use Eloquence, Mappable;
+
+	protected $table        = '011_220_voucher';
+    protected $primaryKey   = 'id_220';
+    protected $suffix       = '220';
+    public $timestamps      = false;
+    protected $fillable     = ['id_301', 'lang_301', 'group_301', 'date_301', 'company_301', 'tin_301', 'gender_301', 'treatment_301', 'state_301', 'name_301', 'surname_301', 'avatar_301', 'birth_date_301', 'email_301', 'phone_301', 'mobile_301', 'user_301', 'password_301', 'active_301', 'confirmed_301', 'country_301', 'territorial_area_1_301', 'territorial_area_2_301', 'territorial_area_3_301', 'cp_301', 'locality_301', 'address_301', 'latitude_301', 'longitude_301', 'custom_field_group_301', 'data_301'];
+    protected $maps         = [
+        'lang'      => \Syscover\Pulsar\Models\Lang::class,
+    ];
+    protected $relationMaps = [];
+    private static $rules   = [
+        'name'      => 'required|between:2,255',
+        'lang'      => 'required',
+        'email'     => 'required|between:2,255|email|unique:009_301_customer,email_301',
+        'user'      => 'required|between:2,255|unique:009_301_customer,user_301',
+        'password'  => 'required|between:4,50|same:repassword'
+    ];
+
+    public static function validate($data, $specialRules)
+    {
+        if(isset($specialRules['emailRule']) && $specialRules['emailRule']) static::$rules['email'] = 'required|between:2,255|email';
+        if(isset($specialRules['userRule']) && $specialRules['userRule'])   static::$rules['user'] = 'required|between:2,255';
+        if(isset($specialRules['passRule']) && $specialRules['passRule'])   static::$rules['password'] = '';
+
+        return Validator::make($data, static::$rules);
+	}
+
+    public function scopeBuilder($query)
+    {
+        return $query->join('001_001_lang', '009_301_customer.lang_301', '=', '001_001_lang.id_001')
+            ->leftJoin('009_300_group', '009_301_customer.group_301', '=', '009_300_group.id_300');
+    }
+
+    /**
+     * Get Lang from user
+     *
+     * @return \Syscover\Pulsar\Models\Lang
+     */
+    public function getLang()
+    {
+        return $this->belongsTo('Syscover\Pulsar\Models\Lang', 'lang_355');
+    }
+
+    /**
+     * Get group from user
+     *
+     * @return \Syscover\Crm\Models\Group
+     */
+    public function getGroup()
+    {
+        return $this->belongsTo(Group::class, 'group_301');
+    }
+
+    /**
+     * Get attachments from customer
+     *
+     * @return mixed
+     */
+    public function getAttachments()
+    {
+        return $this->hasMany('Syscover\Pulsar\Models\Attachment', 'object_016')
+            ->where('001_016_attachment.lang_016', base_lang()->id_001)
+            ->where('001_016_attachment.resource_016', 'crm-customer')
+            ->leftJoin('001_015_attachment_family', '001_016_attachment.family_016', '=', '001_015_attachment_family.id_015')
+            ->orderBy('001_016_attachment.sorting_016');
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password_301;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return $this->{$this->getRememberTokenName()};
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->{$this->getRememberTokenName()} = $value;
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'remember_token_301';
+    }
+
+    /**
+     * Get the e-mail address where password reset links are sent.
+     *
+     * @return string
+     */
+    public function getEmailForPasswordReset()
+    {
+        return $this->email_301;
+    }
+
+    /**
+     * Get the name of the unique identifier for the user.
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName()
+    {
+        return $this->user_301;
+    }
+}
