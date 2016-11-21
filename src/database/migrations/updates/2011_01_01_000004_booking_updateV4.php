bb2840f3721519226b7c9a0c3237c85e4d7012ce
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-11-21 16:44:47 +0100

    minor fix

--- src/database/migrations/updates/2011_01_01_000004_booking_updateV4.php
+++ src/database/migrations/updates/2011_01_01_000004_booking_updateV4.php
@@ -12,7 +12,7 @@ class BookingUpdateV4 extends Migration
      */
     public function up()
     {
-        if(Schema::hasColumn('011_225_booking', 'tax_percentage_225'))
+        if(! Schema::hasColumn('011_225_booking', 'tax_percentage_225'))
         {
             Schema::table('011_225_booking', function (Blueprint $table) {
                 $table->decimal('tax_percentage_225', 10, 2)->after('total_amount_225');
